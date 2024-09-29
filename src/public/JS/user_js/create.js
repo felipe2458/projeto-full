4e3a189f23c369de5c41fc62f0c33b4896f21fcb
felipe2458 <luizfelipealvesdematos10@gmail.com>
2024-09-28 23:57:55 -0300

    Criei uma validação para ver se tem algo no input

--- src/public/JS/user_js/create.js
+++ src/public/JS/user_js/create.js
@@ -1,7 +1,35 @@
 $(function(){
-    const action = $('input[name="action"]');
+    const name = $('#name');
+    const password = $('#password');
+    const password_confirm = $('#password-confirm');
 
-    action.click((e)=>{
-        e.preventDefault();
+    function focusInput(input){
+        const labelID = input.attr('id') + '-label';
+        const label = $('#' + labelID);
+
+        if(input.val().trim().length > 0){
+            label.addClass('focus-input');
+        }else{
+            label.removeClass('focus-input');
+            input.val('');
+        }
+    }
+
+    $(name).focus(function(){
+        focusInput($(this));
+    }).blur(function(){
+        focusInput($(this));
+    });
+
+    $(password).focus(function(){
+        focusInput($(this));
+    }).blur(function(){
+        focusInput($(this));
+    });
+
+    $(password_confirm).focus(function(){
+        focusInput($(this));
+    }).blur(function(){
+        focusInput($(this));
     });
 });
