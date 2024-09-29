d7a71585e8ada3835e379bb588dfec100bb3f0e1
felipe2458 <luizfelipealvesdematos10@gmail.com>
2024-09-29 01:57:24 -0300

    Criando uma forma de verificar se o nome de usuário é valido(parte só do fronte end)

--- src/public/JS/user_js/create.js
+++ src/public/JS/user_js/create.js
@@ -2,6 +2,10 @@ $(function(){
     const name = $('#name');
     const password = $('#password');
     const password_confirm = $('#password-confirm');
+    const root = $(':root');
+    let enviUser = false;
+    let enviPass = false;
+    let enviPassConfirm = false;
 
     function focusInput(input){
         const labelID = input.attr('id') + '-label';
@@ -13,6 +17,31 @@ $(function(){
             label.removeClass('focus-input');
             input.val('');
         }
+    }
+
+    const validateForm = {
+        username: function(){
+            const nameVal = name.val().trim();
+            const errorMsg = $('.erro-msg');
+
+            if(nameVal.length === 0){
+                root.css('--box-shadow-color-input_one', '#8f8f8f');
+                errorMsg.eq(0).text('');
+                enviUser = false;
+            }else if(nameVal.length > 0 && nameVal.length < 10){
+                errorMsg.eq(0).text('O nome tem que ter entre 10 e 100 caracteres.');
+                root.css('--box-shadow-color-input_one', '#a10000');
+            }else if(nameVal.length > 100 ){
+                errorMsg.eq(0).text('O nome tem que ter entre 10 e 100 caracteres.');
+                root.css('--box-shadow-color-input_one', '#a10000');
+            }else if(nameVal.length < 100 && nameVal.length > 10){
+                name.removeClass('error-input');
+                errorMsg.eq(0).text('');
+                root.css('--box-shadow-color-input_one', '#00e000');
+                enviUser = true;
+            }
+        },
+        password: function(){},
     }
 
     $(name).focus(function(){
@@ -31,5 +60,20 @@ $(function(){
         focusInput($(this));
     }).blur(function(){
         focusInput($(this));
+    });
+
+    $(name).on('input' ,function(){
+        validateForm.username();
+    });
+
+    const action = $('input[name="action"]');
+
+    action.click(function(e){
+        if(enviUser && enviPass && enviPassConfirm){
+            return true;
+        }else{
+            e.preventDefault();
+            return false;
+        }
     });
 });
